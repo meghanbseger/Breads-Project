5019968e7573e0ff70dc74b1a9a087afc1310096
Meghan Seger <meghanbmckinney@gmail.com>
2022-03-06 16:09:56 -0600

    edited multiple files

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,12 +4,24 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-  res.send(Bread)
+  res.render('Index',
+    {
+      breads: Bread
+    }
+  )
+// res.send(Bread)
 })
 
-module.exports = breads
+
+
+
+
+
+
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
   res.send(Bread[req.params.arrayIndex])
 })
+
+module.exports = breads
